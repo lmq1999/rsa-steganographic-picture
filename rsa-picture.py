6b7bbd62b965574fd46eff6631b3e53eaf77e7da
leminhquan <quanlm1999@gmail.com>
2021-01-30 10:53:38 +0700

    update stegano embed and deembed

--- rsa-picture.py
+++ rsa-picture.py
@@ -3,21 +3,31 @@ import os,sys
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+import re
 
 from random import randrange, getrandbits
 
-my_img  = Image.open("stegano.jpg")
-pixels = my_img.load()
-print("Size:"+str(my_img.size)+" Format:"+my_img.format)
-plt.imshow(my_img, cmap="gray")
+
+def int_to_bin(rgb):
+    r, g, b = rgb
+    return ('{0:08b}'.format(r),
+            '{0:08b}'.format(g),
+            '{0:08b}'.format(b))
+
+def bin_to_int(rgb):
+
+    r, g, b = rgb
+    return (int(r, 2),
+            int(g, 2),
+            int(b, 2))
 
 #RSA
-
 # STEP 1: Generate Two Large Prime Numbers (p,q) randomly
 
+print("Generating custom RSA key:.....")
 
 def power(a,d,n):
-    ans=1;
+    ans=1
     while d!=0:
         if d%2==1:
             ans=((ans%n)*(a%n))%n
@@ -34,12 +44,12 @@ def MillerRabin(N,d):
     else:
         while(d!=N-1):
             x=((x%N)*(x%N))%N
-            if x==1:
-                return False
-            if x==N-1:
-                return True
-            d<<=1
-    return False
+            if x==1:cover_image  = Image.open("stegano.jpg")
+# cover_image_pixels = cover_image.load()
+# cover_row,cover_col=cover_image.size
+
+
+
 
 
 def is_prime(N,K):
@@ -49,6 +59,7 @@ def is_prime(N,K):
         return False
   
   #Find d such that d*(2^r)=X-1
+
     d=N-1
     while d%2!=0:
         d/=2
@@ -61,11 +72,7 @@ def is_prime(N,K):
 
 
 def generate_prime_candidate(length):
-    # generate random bits
     p = getrandbits(length)
-    # apply a mask to set MSB and LSB to 1
-    # Set MSB to 1 to make sure we have a Number of 1024 bits.
-    # Set LSB to 1 to make sure we get a Odd Number.
     p |= (1 << length - 1) | 1
     return p
 
@@ -88,10 +95,11 @@ print("Q:",Q)
 
 
 #Step 2: Calculate N=P*Q and Euler Totient Function = (P-1)*(Q-1)
+
 N=P*Q
 eulerTotient=(P-1)*(Q-1)
 print("N:",N)
-print("φ:",eulerTotient)
+print("φ(n):",eulerTotient)
 
 
 
@@ -99,7 +107,7 @@ print("φ:",eulerTotient)
 
 def GCD(a,b):
     if a==0:
-        return b;
+        return b
     return GCD(b%a,a)
 
 
@@ -107,7 +115,6 @@ E=generatePrimeNumber(4)
 while GCD(E,eulerTotient)!=1:
     E=generatePrimeNumber(4)
 print("E:",E)
-
 
 
 # Step 4: Find D. 
@@ -150,35 +157,126 @@ D=gcdExtended(E,eulerTotient)
 print("D:",D)
 
 
-row,col=my_img.size
-enc = [[0 for x in range(3000)] for y in range(3000)]
+print("Public key (e:",E,"n:",N,")")
+print("Private key (d:",D,"n:",N,")")
+print("============================")
+
+####################################################################################################################################
+encrypt_img  = Image.open("test.jpeg")
+encrypt_img_pixels = encrypt_img.load()
+# print("Picture infomation:")
+# print("Size:"+str(encrypt_img.size)+" Format:"+encrypt_img.format)
+
+
+encrypt_row,encrypt_col=encrypt_img.size
+enc = [[0 for x in range(encrypt_row)] for y in range(encrypt_col)]
+
 
 #Step 5: Encryption
-for i in range(row):
-  for j in range(col):
-    r,g,b = pixels[i,j]
-    C1=power(r,E,N)
-    C2=power(g,E,N)
-    C3=power(b,E,N)
+print("Encrypting picture with RSA public key (e:",E,"n:",N,")...")
+for i in range(encrypt_row):
+  for j in range(encrypt_col):
+    red,green,blue = encrypt_img_pixels[i,j]
+    C1=power(red,E,N)
+    C2=power(green,E,N)
+    C3=power(blue,E,N)
     enc[i][j]=[C1,C2,C3]
     C1=C1%256
     C2=C2%256
     C3=C3%256
-    pixels[i,j]=(C1,C2,C3)
+    encrypt_img_pixels[i,j]=(C1,C2,C3)
 
-my_img.save("./rsa_encrypt_output.png")
+encrypt_img.save("./rsa_encrypt_output.png")
+print("RSA_encrypted picture complete, save at ./rsa_encrypt_output.png")
+print("============================")
+####################################################################################################################################
+#Step B: Embedding image
+
+cover_image  = Image.open("stegano.jpg")
+cover_image_pixels = cover_image.load()
+cover_row,cover_col=cover_image.size
+
+di,dj=0,0
+
+def embebding(di,dj,count,red_b,green_b,blue_b):
+    d_red,d_green,d_blue = int_to_bin(cover_image_pixels[di,dj])
+    d_red = d_red[0:6]+red_b[count]
+    d_green = d_green[0:6]+green_b[count]
+    d_blue = d_blue[0:6]+blue_b[count]
+    rgb=(d_red, d_green, d_blue)
+    d_red, d_green, d_blue = bin_to_int(rgb)
+    cover_image_pixels[di,dj]=(d_red, d_green, d_blue)
+    return di, dj
+
+print("Embemding encrypted picture to cover picture...")
+for ei in range(encrypt_row):
+  for ej in range(encrypt_col):
+    e_red,e_green,e_blue = int_to_bin(encrypt_img_pixels[ei,ej])
+    red_b = re.findall('..',e_red)
+    green_b = re.findall('..',e_green)
+    blue_b = re.findall('..',e_blue)
+    for count in range(4):
+        di,dj=embebding(di,dj,count,red_b,green_b,blue_b)
+        dj+=1
+        if (dj == cover_col):
+            dj=0
+            di+=1
+cover_image.save("./embed_cover.png")
+print("Finish embemd encrypted picture to cover picture...")
+print("============================")
+####################################################################################################################################
+#Step B: De-embedding image
+
+embeded_image=Image.open("embed_cover.png")
+embeded_image_pixels = embeded_image.load()
+embeded_image_row,embeded_image_col=embeded_image.size
+
+dei,dej=0,0
+def de_embebed(dei,dej):
+    dej_red,dej_green,dej_blue = int_to_bin(embeded_image_pixels[dei,dej])
+    dej_red = dej_red[6:8]
+    dej_green = dej_green[6:8]
+    dej_blue = dej_blue[6:8]
+    return dei, dej, dej_red, dej_green, dej_blue
+
+new_image = Image.new(encrypt_img.mode, encrypt_img.size)
+pixels_new = new_image.load()
+new_image_row,new_image_col=new_image.size
+
+for ni in range(new_image_row):
+    for nj in range(new_image_col):
+        dei_red,dei_green,dei_blue="","",""
+        for count in range(4):
+            dei,dej,dei_red_b,dei_green_b,dei_blue_b=de_embebed(dei,dej)
+            dei_red+=dei_red_b
+            dei_green+=dei_green_b
+            dei_blue+=dei_blue_b
+            dej+=1
+            if (dej == embeded_image_col):
+                dej=0
+                dei+=1
+        rgb=(dei_red,dei_green,dei_blue)
+        dei_red,dei_green,dei_blue = bin_to_int(rgb)
+        pixels_new[ni,nj]=(dei_red,dei_green,dei_blue)
+
+new_image.save("./de_embebed.png")
 
 
 
-
-
+####################################################################################################################################
 #Step 6: Decryption
-for i in range(row):
-  for j in range(col):
-    r,g,b=enc[i][j]
-    M1=power(r,D,N)
-    M2=power(g,D,N)
-    M3=power(b,D,N)
-    pixels[i,j]=(M1,M2,M3)
+decrypted_image=Image.open("de_embebed.png")
+decrypted_image_pixels = decrypted_image.load()
+decrypted_image_row,decrypted_image_col=decrypted_image.size
 
-my_img.save("./rsa_decrypt_output.png")
+print("Decrypting picture with RSA private key (d:",D,"n:",N,")...")
+for i in range(decrypted_image_row):
+  for j in range(decrypted_image_col):
+    red,green,blue =enc[i][j]
+    M1=power(red,D,N)
+    M2=power(green,D,N)
+    M3=power(blue,D,N)
+    decrypted_image_pixels[i,j]=(M1,M2,M3)
+
+decrypted_image.save("./rsa_decrypt_output.png")
+print("RSA_decrypted picture complete, save at ./rsa_decrypt_output.png")
